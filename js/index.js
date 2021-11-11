c906694d04167c4bf28f8fce8677e4e4f45a2707
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2021-11-11 20:08:30 +0530

    A
    New Comments Added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 let inputDir = {x: 0, y: 0}; 
-const foodEatingSound = new Audio('music/food.mp3');
-const gameOverSound = new Audio('music/gameover.mp3');
-const moveSound = new Audio('music/move.wav');
-const musicSound = new Audio('music/music.mp3');
+
 let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
@@ -12,10 +9,17 @@ let snakeBodyArray = [
 
 food = {x: 6, y: 7};
 
+// fadding function varibale 
 const BOARD_OPACITY_REDUCTION_RATE = 0.02;
-const BOARD_MIN_OPACITY = 0;
-const BOARD_MAX_OPACITY = 1;
-let boardCurrentOpacity = BOARD_MAX_OPACITY;
+const BOARD_MIN_OPACITY = 0; // minimum opacity of the board is to be 0
+const BOARD_MAX_OPACITY = 1; // maximum opacity of the board is to be 1
+let boardCurrentOpacity = BOARD_MAX_OPACITY; // initialy the board opacity will be maximum meaning 1
+
+// sound used in the game
+const foodEatingSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const movingSound = new Audio('music/move.wav');
+const backgroundMusicSound = new Audio('music/music.mp3');
 
 const GAME_STATUS = {
     STARTED: "STARTED",
@@ -24,6 +28,7 @@ const GAME_STATUS = {
 
 let gameStatus = GAME_STATUS.GAME_OVER;
 
+// for fps of the game
 function main(ctime) {
     window.requestAnimationFrame(main);
     if((ctime - lastPaintTime)/1000 < 1/speed){
@@ -33,31 +38,34 @@ function main(ctime) {
     mainGameFunction();
 }
 
-function isCollide(snake) {
+// various game ending conditions
+function EndingGameFunction(snake) {
     for (let i = 1; i < snakeBodyArray.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){ // colliding with itself with the snake
             return true;
         }
     }
-    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){ // colliding with the boundry of the game
         return true;
     }
         
-    return false;
-}
+    return false; // no ending game condition
+} 
 
+// fadding various function
+// making the game opacity to be minimum when the boards in not visible
 function gameBoardDisappeared() {
     return boardCurrentOpacity == BOARD_MIN_OPACITY;
 }
-
+// game ending condition when the board opacity becomes zero
 function shouldEndGame(snakeBodyArray) {
-    return isCollide(snakeBodyArray) || gameBoardDisappeared();
+    return EndingGameFunction(snakeBodyArray) || gameBoardDisappeared();
 }
-
+// recduing the game boards opacity with the rate 
 function reduceBoardOpacity() {
     updateBoardOpacity(boardCurrentOpacity - BOARD_OPACITY_REDUCTION_RATE);
 }
-
+// 
 function updateBoardOpacity(opacity) {
     opacity = Math.min(Math.max(opacity, BOARD_MIN_OPACITY), BOARD_MAX_OPACITY);
     if (boardCurrentOpacity !== opacity) {
@@ -66,70 +74,84 @@ function updateBoardOpacity(opacity) {
     }
 }
 
+
 function mainGameFunction(){
+
+    // condition for game end
     if(shouldEndGame(snakeBodyArray)){
+
         gameStatus = GAME_STATUS.GAME_OVER;
-        updateBoardOpacity(BOARD_MAX_OPACITY);
+        updateBoardOpacity(BOARD_MAX_OPACITY); // resetting the opacity
         gameOverSound.play();
-        musicSound.pause();
+        backgroundMusicSound.pause(); // pausing the background music because the game has ended
+        
         inputDir =  {x: 0, y: 0}; 
-        alert("Oops Turned In Late! Press any key to play again!");
-        snakeBodyArray = [{x: 13, y: 15}];
-        musicSound.play();
+        alert("Oops Turned In Late! Press any key to play again!"); // end game popup
+        snakeBodyArray = [{x: 13, y: 15}]; // resetting the snake position after the game ends
+        backgroundMusicSound.play();
         score = 0; 
     }
-
+    // if food is eaten
     if(snakeBodyArray[0].y === food.y && snakeBodyArray[0].x ===food.x){
         foodEatingSound.play();
-        updateBoardOpacity(BOARD_MAX_OPACITY);
+
+        updateBoardOpacity(BOARD_MAX_OPACITY); // resetting the opacity
         score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
+        if(score>hiscoreval){ // local storage for storing the highest score of the game
+            hiscoreval = score; 
             localStorage.setItem("Highest Submission", JSON.stringify(hiscoreval));
             hiscoreBox.innerHTML = "Highest Submission: " + hiscoreval;
         }
+
+        // increment of the latest score
         scoreBox.innerHTML = "Assignments Submitted: " + score;
-        snakeBodyArray.unshift({x: snakeBodyArray[0].x + inputDir.x, y: snakeBodyArray[0].y + inputDir.y});
-        let a = 2;
+        snakeBodyArray.unshift({x: snakeBodyArray[0].x + inputDir.x, y: snakeBodyArray[0].y + inputDir.y}); // adding clone head body segment to the body of the snake 
+        let a = 2; // grid number for spawing the food
         let b = 16;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+
+        // new location of the food spawn
+        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())} // will just generate a random number between a and b 
+    
     } else {
         if (gameStatus == GAME_STATUS.STARTED) {
             reduceBoardOpacity();
         }
     }
 
-    for (let i = snakeBodyArray.length - 2; i>=0; i--) { 
+    for (let i = snakeBodyArray.length - 2; i>=0; i--) { // movement of the snake with just shifting the body segments one by one
         snakeBodyArray[i+1] = {...snakeBodyArray[i]};
     }
 
     snakeBodyArray[0].x += inputDir.x;
     snakeBodyArray[0].y += inputDir.y;
     board.innerHTML = "";
-    snakeBodyArray.forEach((e, index)=>{
-        snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = e.y;
-        snakeElement.style.gridColumnStart = e.x;
 
+    // adding of the food in the body of the snake and displaying the snake
+    snakeBodyArray.forEach((e, index)=>{
+        
+        snakeBodyElement = document.createElement('div');
+        snakeBodyElement.style.gridRowStart = e.y;
+        snakeBodyElement.style.gridColumnStart = e.x;
+        
         if(index === 0){
-            snakeElement.classList.add('head');
+            snakeBodyElement.classList.add('head');
         }
         else{
-            snakeElement.classList.add('snake');
+            snakeBodyElement.classList.add('snake');
         }
-        board.appendChild(snakeElement);
+        board.appendChild(snakeBodyElement);
     });
-    // Display the food
-    foodElement = document.createElement('div');
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food')
-    board.appendChild(foodElement);
+
+    foodMainElement = document.createElement('div');
+    foodMainElement.style.gridRowStart = food.y;
+    foodMainElement.style.gridColumnStart = food.x;
+    foodMainElement.classList.add('food')
+    board.appendChild(foodMainElement);
 
 
 }
-
-musicSound.play();
+// for score setting either null or the high score of the best game played
+backgroundMusicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval = 0;
@@ -140,14 +162,16 @@ else{
     hiscoreBox.innerHTML = "Highest Submission: " + hiscore;
 }
 
+// for movement input from the user for the game
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
     inputDir = {x: 0, y: 1}
-    moveSound.play();
-    if (gameStatus == GAME_STATUS.GAME_OVER) {
+    movingSound.play();
+    if (gameStatus == GAME_STATUS.GAME_OVER) { // changing the game status because we want the opacity to be man in the starting of the game
         gameStatus = GAME_STATUS.STARTED;
     }
-    switch (e.key) {
+
+    switch (e.key) { // all the 4 arrow key movement input for the snake and ther respective effect in the movement in the game
         case "ArrowUp":
             console.log("ArrowUp");
             inputDir.x = 0;
@@ -159,13 +183,12 @@ window.addEventListener('keydown', e =>{
             inputDir.x = 0;
             inputDir.y = 1;
             break;
-
+            
         case "ArrowLeft":
             console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
             break;
-
         case "ArrowRight":
             console.log("ArrowRight");
             inputDir.x = 1;
